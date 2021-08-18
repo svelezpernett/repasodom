24e6e6c0eb0518c187464e5a9b0c7d604a79ae26
svelezpernett <saniagovp16@gmail.com>
2021-08-18 13:39:04 -0500

    se programo un boton en js con la funcion para cambiar cancion e imagen(No se devuelve)

--- controlador.js
+++ controlador.js
@@ -1,1 +1,22 @@
 console.log("Hola mundo");
+
+var boton = document.getElementById("boton");
+
+
+function cambiarCancion()
+{
+    var cancion1 = document.getElementById("cancion1");
+
+    if(cancion1.src="audio/kiñoDetrote.mp3")
+    {
+        cancion1.src="audio/kiñoGusfraba.mp3";
+        document.getElementById("imagen1").src="img/img2.jpg";
+    }
+    else
+    {
+       cancion1.src="audio/kiñoDetrote.mp3";
+       document.getElementById("imagen1").src="img/img1.jpg";  
+    }
+}
+
+boton.addEventListener("click", cambiarCancion);
